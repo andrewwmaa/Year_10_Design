0f445f8c6fec2fceb5b631570b2b7ab1a10d1499
Andrew Ma <54967788+thealpes@users.noreply.github.com>
2020-11-04 16:21:36 -0500

    Create dropdown.py

---
+++ dropdown.py
@@ -0,0 +1,1 @@
+import tkinter as tk  OptionList = [ "This", "is", "a", "list" ] #construct app = tk.Tk()  app.geometry('100x200') #config variable = tk.StringVar(app) variable.set(OptionList[0])  opt = tk.OptionMenu(app, variable, *OptionList) opt.config(width=90, font=('Helvetica', 12)) #pack opt.pack()  app.mainloop()
