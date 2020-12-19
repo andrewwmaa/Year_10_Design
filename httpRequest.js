a52c35ae28e459e1f89a0922620cf01e20494c67
Andrew Ma <andrew.ma@ucc.on.ca>
2020-12-18 19:24:10 -0500

    update

--- httpRequest.js
+++
@@ -1,8 +0,0 @@
-const Http = new XMLHttpRequest();
-const url='https://api.themoviedb.org/3/movie/550?api_key=3fb7f922af46a3a269fd99898af94702';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-  }
